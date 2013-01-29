36778d11c6237f8fd96aa7a48be102c6bf59983f
Lajos Koszti <ajnasz@ajnasz.hu>
2013-01-29 20:55:17 +0100

    Download jquery klass

--- bench/bench.js
+++ bench/bench.js
@@ -40,35 +40,74 @@ function runTests() {
 
 function getJSClasses(callback) {
     "use strict";
-    var classes, http, fs, count;
+    var classes, http, https, fs, count;
     classes = [
         {
             name: 'jsface',
             url: 'http://dl.dropbox.com/u/7677927/oop-benchmark/lib/jsface.js'
+        },
+        {
+            name: 'jqueryClass',
+            secure: true,
+            url: 'https://raw.github.com/rosamez/jQuery.klass/master/jquery.klass.js',
+            postprocess: function (data) {
+                var str = data.toString('utf8').split('\n'),
+                    len = str.length,
+                    i = 0;
+                while (i < len) {
+                    if (str[i].indexOf('MooTools.BOM =') > -1) {
+                        while (str[i].indexOf('}())') === -1) {
+                            str[i] = '// ' + str[i];
+                            i += 1;
+                        }
+                        str[i] = '// ' + str[i];
+                    }
+                    i += 1;
+                }
+                return str.join('\n');
+            }
         }
     ];
 
     http = require('http');
+    https = require('https');
     fs = require('fs');
 
     count = 0;
 
+    function onFileDownloaded() {
+        count += 1;
+        if (count >= classes.length) {
+            callback();
+
+            classes.forEach(function (cl) {
+                fs.unlink(__dirname + '/' + cl.name + '.js');
+            });
+        }
+    }
+
     classes.forEach(function (cl) {
-        var file = fs.createWriteStream(__dirname + '/' + cl.name + '.js');
+        var file = fs.createWriteStream(__dirname + '/' + cl.name + '.js'), get;
 
         file.on('close', function () {
-            count += 1;
 
-            if (count >= classes.length) {
-                callback();
-
-                classes.forEach(function (cl) {
-                    fs.unlink(__dirname + '/' + cl.name + '.js');
+            if (cl.postprocess) {
+                fs.readFile(__dirname + '/' + cl.name + '.js', function (err, data) {
+                    var processed = cl.postprocess(data);
+                    fs.writeFile(__dirname + '/' + cl.name + '.js', processed);
+                    onFileDownloaded();
                 });
+            } else {
+                onFileDownloaded();
             }
         });
 
-        http.get(cl.url, function (response) {
+        if (cl.secure) {
+            get = https.get;
+        } else {
+            get = http.get;
+        }
+        get(cl.url, function (response) {
             response.pipe(file);
         }).on('error', function (er) {
             console.log(er);
